c88920f2c1db1a8fcee1784d3ab3b05575dcc702
wangyq <985759262@qq.com>
2022-05-06 15:13:06 +0800

    test

--- database/database_test.go
+++ database/database_test.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/gorm"
 
 	"github.com/jdxj/study-gorm/er"
 	"github.com/jdxj/study-gorm/key"
@@ -40,4 +42,24 @@ func TestSelect(t *testing.T) {
 		t.Fatalf("%s\n", err)
 	}
 	fmt.Printf("%+v\n", user)
+}
+
+func TestInsert(t *testing.T) {
+	user := &er.User{
+		Model: gorm.Model{
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		Name:   "123",
+		Gender: 4,
+		Phone:  "567",
+	}
+
+	_, err := db.ExecContext(
+		context.Background(),
+		"insert into users (created_at,updated_at,name,gender,phone) values (?,?,?,?,?)",
+		user.CreatedAt, user.UpdatedAt, user.Name, user.Gender, user.Phone)
+	if err != nil {
+		t.Fatalf("%s\n", err)
+	}
 }
